b5b5ebb3d69ea47fff09726748bf4f544c4f4d4b
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-09-06 01:12:27 +0530

    finished addEventListener for install

---
+++ public/serviceworker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+// Install SW
+self.addEventListener("install", (e) => {
+  const preCache = async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    console.log("Opened cache");
+    return cache.addAll(urlsToCache);
+  };
+
+  e.waitUntil(preCache());
+
+  //   e.waitUntil(caches.open(CACHE_NAME)).then((cache) => {
+  //     console.log("Opened cache");
+
+  //     return cache.addAll(urlsToCache);
+  //   });
+});
+
+// Listen for requests
+self.addEventListener("fetch", (e) => {});
+
+// Activate the SW
+self.addEventListener("activate", (e) => {});
